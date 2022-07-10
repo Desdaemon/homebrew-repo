6b02949d453759e156e761d88cdf9abb1bbc74ce
Viet Dinh <54ckb0y789@gmail.com>
2022-07-10 10:36:33 -0400

    flutter_rust_bridge_codegen 1.37.2

--- Formula/flutter_rust_bridge_codegen.rb
+++ Formula/flutter_rust_bridge_codegen.rb
@@ -1,8 +1,8 @@
 class FlutterRustBridgeCodegen < Formula
   desc "High-level memory-safe binding generator for Flutter/Dart <-> Rust"
   homepage "https://fzyzcjy.github.io/flutter_rust_bridge"
-  url "https://github.com/fzyzcjy/flutter_rust_bridge/archive/refs/tags/v1.37.1.tar.gz"
-  sha256 "0e3c1c8346ad09d3c97416c76372770b980de4dbbae5c26ce48ce9797a554329"
+  url "https://github.com/fzyzcjy/flutter_rust_bridge/archive/refs/tags/v1.37.2.tar.gz"
+  sha256 "499a9c63a0c381516a3d343062123cca044e9cf4368ee8aa09fcb1a2f96b3049"
   license "MIT"
 
   depends_on "rust" => :build
