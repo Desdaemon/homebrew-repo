dfbeebe6d0d24939ebb68e1663af6c42acafa0f2
Viet Dinh <36768030+Desdaemon@users.noreply.github.com>
2022-07-10 14:56:14 +0000

    flutter_rust_bridge_codegen: add 1.37.2 bottle.

--- Formula/flutter_rust_bridge_codegen.rb
+++ Formula/flutter_rust_bridge_codegen.rb
@@ -5,6 +5,12 @@ class FlutterRustBridgeCodegen < Formula
   sha256 "499a9c63a0c381516a3d343062123cca044e9cf4368ee8aa09fcb1a2f96b3049"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Desdaemon/homebrew-repo/releases/download/flutter_rust_bridge_codegen-1.37.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "d1b616bc137f70ac82bbf6bde623876f73c09999795a6de6158bc8adfc5f2f18"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4a9d8c5efeb94057cc21b46dc5f1c5fa40949b2ef74e2fc61eb7829021e1299e"
+  end
+
   depends_on "rust" => :build
 
   def install
