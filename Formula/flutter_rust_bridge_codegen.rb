8223ca4d9b4173904ca6ce0a2fc0be2ece168520
fzyzcjy2 <92035302+fzyzcjy2@users.noreply.github.com>
2024-05-26 13:28:19 +0800

    flutter_rust_bridge_codegen 2.0.0-dev.36
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/flutter_rust_bridge_codegen.rb
+++ Formula/flutter_rust_bridge_codegen.rb
@@ -1,8 +1,8 @@
 class FlutterRustBridgeCodegen < Formula
   desc "High-level memory-safe binding generator for Flutter/Dart <-> Rust"
   homepage "https://fzyzcjy.github.io/flutter_rust_bridge"
-  url "https://github.com/fzyzcjy/flutter_rust_bridge/archive/refs/tags/v2.0.0-dev.3.tar.gz"
-  sha256 "4bbbd082d6db92240e4e00017cf0485517142a0ac036b82de532ca5d21882c19"
+  url "https://github.com/fzyzcjy/flutter_rust_bridge/archive/refs/tags/v2.0.0-dev.36.tar.gz"
+  sha256 "f79c0201194281cb99e7ba3efffa2daa4257c19ba85fbf45ade91023dbc2c4e8"
   license "MIT"
 
   bottle do
